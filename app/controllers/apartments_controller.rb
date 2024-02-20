9bbab94cec69b6e7564ffb92b6bc8ae70287fa22
irvinmoore <irvin.moore7@gmail.com>
2024-02-20 11:48:24 -0800

    Update to Apartments Controller RB

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,24 +1,18 @@
 class ApartmentsController < ApplicationController
   def index
-  apartments = Apartment.all
-  render json: apartments
-end
-  apartments = Apartment.all
-  render json: apartments
-end
-def create
-  apartment = Apartment.create(apartment_params)
-  if apartment.valid?
-    render json: apartment
-  else
-    render json: apartment.errors, status: 422
+    apartments = Apartment.all
+    render json: apartments
+  end
+  def create
+   apartment = Apartment.create(apartment_params)
+    if apartment.valid?
+     render json: apartment
+    else
+     render json: apartment.errors, status: 422
   end
 end
-
 private
-def apartment_params
-  params.require(:apartment).permit(:street, :city, :state, :unit, :square_footage, :price, :bedrooms, :bathrooms, :pets, :image, :user_id)
-end
+  def apartment_params
+    params.require(:apartment).permit(:street, :city, :state, :unit, :square_footage, :price, :bedrooms, :bathrooms, :pets, :image, :user_id)
+  end
 
-end
-end
